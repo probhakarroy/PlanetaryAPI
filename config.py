9eafa2b80fca21f46178d69db4d4210e40d70666
Probhakar Roy <probhakarroy3110@gmail.com>
2020-08-04 22:15:59 +0530

    * deployment ready

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 
 class Config:
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'shoot will allow rock'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'random string from xkcd password generator.'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///planets.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.mailtrap.io'
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
